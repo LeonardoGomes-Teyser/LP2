0ef4b1b905a18ada17da95368f1059e568944ed1
LeonardoGomes-Teyser <78798625+LeonardoGomes-Teyser@users.noreply.github.com>
2021-03-08 14:13:39 -0300

    Add files via upload

---
+++ ATIVIDADE3/Triângulos/Triângulos/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace Triângulos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_a = new System.Windows.Forms.TextBox();
+            this.txt_c = new System.Windows.Forms.TextBox();
+            this.txt_b = new System.Windows.Forms.TextBox();
+            this.btn_verificar = new System.Windows.Forms.Button();
+            this.txt_resultado = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lado A:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lado B:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Lado C:";
+            // 
+            // txt_a
+            // 
+            this.txt_a.Location = new System.Drawing.Point(81, 42);
+            this.txt_a.Name = "txt_a";
+            this.txt_a.Size = new System.Drawing.Size(100, 22);
+            this.txt_a.TabIndex = 3;
+            // 
+            // txt_c
+            // 
+            this.txt_c.Location = new System.Drawing.Point(81, 143);
+            this.txt_c.Name = "txt_c";
+            this.txt_c.Size = new System.Drawing.Size(100, 22);
+            this.txt_c.TabIndex = 4;
+            // 
+            // txt_b
+            // 
+            this.txt_b.Location = new System.Drawing.Point(81, 91);
+            this.txt_b.Name = "txt_b";
+            this.txt_b.Size = new System.Drawing.Size(100, 22);
+            this.txt_b.TabIndex = 5;
+            // 
+            // btn_verificar
+            // 
+            this.btn_verificar.Location = new System.Drawing.Point(6, 41);
+            this.btn_verificar.Name = "btn_verificar";
+            this.btn_verificar.Size = new System.Drawing.Size(100, 116);
+            this.btn_verificar.TabIndex = 6;
+            this.btn_verificar.Text = "VERIFICAR";
+            this.btn_verificar.UseVisualStyleBackColor = true;
+            this.btn_verificar.Click += new System.EventHandler(this.btn_verificar_Click);
+            // 
+            // txt_resultado
+            // 
+            this.txt_resultado.Enabled = false;
+            this.txt_resultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_resultado.Location = new System.Drawing.Point(269, 246);
+            this.txt_resultado.Name = "txt_resultado";
+            this.txt_resultado.Size = new System.Drawing.Size(364, 30);
+            this.txt_resultado.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(112, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 49);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "LIMPAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(112, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 54);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "SAIR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(373, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(157, 25);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Tipo do triângulo";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(194, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(505, 29);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "DESCUBRA A FORMA DO SEU TRIÂNGULO!";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_b);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txt_a);
+            this.groupBox1.Controls.Add(this.txt_c);
+            this.groupBox1.Location = new System.Drawing.Point(12, 150);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 181);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dados do Triângulo:";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_verificar);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(681, 144);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(204, 186);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Controles:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Triângulos.Properties.Resources.primeira_classificacao_de_triangulos;
+            this.pictureBox1.Location = new System.Drawing.Point(199, 336);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 271);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(950, 696);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_resultado);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_a;
+        private System.Windows.Forms.TextBox txt_c;
+        private System.Windows.Forms.TextBox txt_b;
+        private System.Windows.Forms.Button btn_verificar;
+        private System.Windows.Forms.TextBox txt_resultado;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ATIVIDADE3/Triângulos/Triângulos/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Triângulos
+{
+    public partial class Form1 : Form
+    {
+        double A, B, C;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txt_resultado.Clear();
+            txt_a.Clear();
+            txt_b.Clear();
+            txt_c.Clear();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_verificar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txt_a.Text) || string.IsNullOrEmpty(txt_b.Text) || string.IsNullOrEmpty(txt_c.Text)) 
+            {
+                txt_resultado.Text = "Preencha os campos em branco";
+            }
+            else
+            {
+                if (double.TryParse(txt_a.Text, out A) && double.TryParse(txt_b.Text, out B) && double.TryParse(txt_c.Text, out C))
+                {
+                    if ((A == B) && (B == C) && (A == C))
+                        txt_resultado.Text = "Equilatero";
+                    else
+                        txt_resultado.Text = "Isoceles";
+                    if ((A != B) && (B != C) && (A != C))
+                        txt_resultado.Text = "Escaleno";
+                }
+                else
+                    txt_resultado.Text = "Digite números no campo";
+
+            }
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
