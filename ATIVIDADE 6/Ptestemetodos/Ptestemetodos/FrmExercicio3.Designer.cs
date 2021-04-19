2aa310e706fc70ed417fd29c00f4e75c120cae67
LeonardoGomes-Teyser <78798625+LeonardoGomes-Teyser@users.noreply.github.com>
2021-04-19 11:38:19 -0300

    Add files via upload

---
+++ ATIVIDADE 6/Ptestemetodos/Ptestemetodos/FrmExercicio3.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace Ptestemetodos
+{
+    partial class FrmExercicio3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtPalavra1 = new System.Windows.Forms.TextBox();
+            this.TxtPalavra2 = new System.Windows.Forms.TextBox();
+            this.btnRemover1 = new System.Windows.Forms.Button();
+            this.btnRemover2 = new System.Windows.Forms.Button();
+            this.btnInverte = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(10, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Palavra 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(10, 77);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Palavra 2";
+            // 
+            // TxtPalavra1
+            // 
+            this.TxtPalavra1.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtPalavra1.Location = new System.Drawing.Point(153, 13);
+            this.TxtPalavra1.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtPalavra1.Name = "TxtPalavra1";
+            this.TxtPalavra1.Size = new System.Drawing.Size(419, 34);
+            this.TxtPalavra1.TabIndex = 2;
+            // 
+            // TxtPalavra2
+            // 
+            this.TxtPalavra2.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtPalavra2.Location = new System.Drawing.Point(153, 76);
+            this.TxtPalavra2.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtPalavra2.Name = "TxtPalavra2";
+            this.TxtPalavra2.Size = new System.Drawing.Size(419, 34);
+            this.TxtPalavra2.TabIndex = 3;
+            // 
+            // btnRemover1
+            // 
+            this.btnRemover1.BackColor = System.Drawing.SystemColors.Control;
+            this.btnRemover1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRemover1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemover1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnRemover1.Location = new System.Drawing.Point(16, 160);
+            this.btnRemover1.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRemover1.Name = "btnRemover1";
+            this.btnRemover1.Size = new System.Drawing.Size(244, 84);
+            this.btnRemover1.TabIndex = 4;
+            this.btnRemover1.Text = "Remover ocorrências ";
+            this.btnRemover1.UseVisualStyleBackColor = false;
+            this.btnRemover1.Click += new System.EventHandler(this.btnRemover1_Click);
+            // 
+            // btnRemover2
+            // 
+            this.btnRemover2.BackColor = System.Drawing.SystemColors.Control;
+            this.btnRemover2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRemover2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemover2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnRemover2.Location = new System.Drawing.Point(337, 161);
+            this.btnRemover2.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRemover2.Name = "btnRemover2";
+            this.btnRemover2.Size = new System.Drawing.Size(235, 83);
+            this.btnRemover2.TabIndex = 5;
+            this.btnRemover2.Text = "Remover ocorrência (Replace)";
+            this.btnRemover2.UseVisualStyleBackColor = false;
+            this.btnRemover2.Click += new System.EventHandler(this.btnRemover2_Click);
+            // 
+            // btnInverte
+            // 
+            this.btnInverte.BackColor = System.Drawing.SystemColors.Control;
+            this.btnInverte.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnInverte.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInverte.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnInverte.Location = new System.Drawing.Point(168, 271);
+            this.btnInverte.Margin = new System.Windows.Forms.Padding(4);
+            this.btnInverte.Name = "btnInverte";
+            this.btnInverte.Size = new System.Drawing.Size(244, 92);
+            this.btnInverte.TabIndex = 6;
+            this.btnInverte.Text = "Inverte";
+            this.btnInverte.UseVisualStyleBackColor = false;
+            this.btnInverte.Click += new System.EventHandler(this.btnInverte_Click);
+            // 
+            // FrmExercicio3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(832, 576);
+            this.Controls.Add(this.btnInverte);
+            this.Controls.Add(this.btnRemover2);
+            this.Controls.Add(this.btnRemover1);
+            this.Controls.Add(this.TxtPalavra2);
+            this.Controls.Add(this.TxtPalavra1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmExercicio3";
+            this.Text = "FrmExercicio3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtPalavra1;
+        private System.Windows.Forms.TextBox TxtPalavra2;
+        private System.Windows.Forms.Button btnRemover1;
+        private System.Windows.Forms.Button btnRemover2;
+        private System.Windows.Forms.Button btnInverte;
+    }
+}
---
+++ ATIVIDADE 6/Ptestemetodos/Ptestemetodos/FrmExercicio4.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace Ptestemetodos
+{
+    partial class FrmExercicio4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtfTexto = new System.Windows.Forms.RichTextBox();
+            this.btnNumeros = new System.Windows.Forms.Button();
+            this.btnLetras = new System.Windows.Forms.Button();
+            this.btnBranco = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtfTexto
+            // 
+            this.rtfTexto.Font = new System.Drawing.Font("Segoe UI Symbol", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtfTexto.Location = new System.Drawing.Point(127, 75);
+            this.rtfTexto.Margin = new System.Windows.Forms.Padding(4);
+            this.rtfTexto.Name = "rtfTexto";
+            this.rtfTexto.Size = new System.Drawing.Size(479, 40);
+            this.rtfTexto.TabIndex = 0;
+            this.rtfTexto.Text = "";
+            // 
+            // btnNumeros
+            // 
+            this.btnNumeros.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNumeros.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNumeros.Location = new System.Drawing.Point(127, 180);
+            this.btnNumeros.Margin = new System.Windows.Forms.Padding(4);
+            this.btnNumeros.Name = "btnNumeros";
+            this.btnNumeros.Size = new System.Drawing.Size(160, 78);
+            this.btnNumeros.TabIndex = 1;
+            this.btnNumeros.Text = "Numeros";
+            this.btnNumeros.UseVisualStyleBackColor = true;
+            this.btnNumeros.Click += new System.EventHandler(this.btnNumeros_Click);
+            // 
+            // btnLetras
+            // 
+            this.btnLetras.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLetras.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLetras.Location = new System.Drawing.Point(423, 180);
+            this.btnLetras.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLetras.Name = "btnLetras";
+            this.btnLetras.Size = new System.Drawing.Size(183, 78);
+            this.btnLetras.TabIndex = 2;
+            this.btnLetras.Text = "Letras";
+            this.btnLetras.UseVisualStyleBackColor = true;
+            this.btnLetras.Click += new System.EventHandler(this.btnLetras_Click);
+            // 
+            // btnBranco
+            // 
+            this.btnBranco.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBranco.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBranco.Location = new System.Drawing.Point(256, 282);
+            this.btnBranco.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBranco.Name = "btnBranco";
+            this.btnBranco.Size = new System.Drawing.Size(190, 90);
+            this.btnBranco.TabIndex = 3;
+            this.btnBranco.Text = "Espaço em Branco";
+            this.btnBranco.UseVisualStyleBackColor = true;
+            this.btnBranco.Click += new System.EventHandler(this.btnBranco_Click);
+            // 
+            // FrmExercicio4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(716, 418);
+            this.Controls.Add(this.btnBranco);
+            this.Controls.Add(this.btnLetras);
+            this.Controls.Add(this.btnNumeros);
+            this.Controls.Add(this.rtfTexto);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(734, 465);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(734, 465);
+            this.Name = "FrmExercicio4";
+            this.Text = "FrmExercicio4";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtfTexto;
+        private System.Windows.Forms.Button btnNumeros;
+        private System.Windows.Forms.Button btnLetras;
+        private System.Windows.Forms.Button btnBranco;
+    }
+}
